5454b23cdf98d6db6dab451370f2efb41819e93b
Graham Bygrave <graham@bygrave.org>
2022-10-11 15:48:44 +0100

    Create Google Proxy Example.  Closes #1.

---
+++ src/main/java/com/sfg/gateway/config/GoogleConfigs.java
@@ -0,0 +1,23 @@
+package com.sfg.gateway.config;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfigs {
+
+    @Bean
+    public RouteLocator googleConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("google",
+                       r -> r.path("/search2")
+                               .filters(f -> f.rewritePath("/search2(?<segment>/?.*)", "/${segment}"))
+                               .uri("https://google.com"))
+                .build();
+    }
+}
